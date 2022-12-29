787b4625e41466b70092463d1324eda04a0b15c2
raunakp777 <72513266+raunakp777@users.noreply.github.com>
2022-12-29 14:09:19 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -18,7 +18,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-sl.multiselect("Pick some fruits:", list(my_fruit_list.index)['Avacado', 'Strawberries'])
+sl.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado', 'Strawberries'])
 
 
 # Display the table on the page.
