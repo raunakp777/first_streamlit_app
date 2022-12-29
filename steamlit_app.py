bbbd314c1c40f35fd13dadd5bae27d8548ba0ac9
raunakp777 <72513266+raunakp777@users.noreply.github.com>
2022-12-29 14:12:06 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -20,6 +20,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 sl.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 
 # Display the table on the page.
 sl.dataframe(my_fruit_list)
