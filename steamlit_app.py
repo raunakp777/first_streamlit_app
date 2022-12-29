5d06e01d81154a1333c74ebaefdf9d0f314631d1
raunakp777 <72513266+raunakp777@users.noreply.github.com>
2022-12-29 13:56:24 -0500

    Update steamlit_app.py
    added text and header to app

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as sl
 
 sl.title("My Parents' Healthy New Diner")
+sl.header('Breakfast Menu')
+sl.text('Omega 3 & Blueberry Oatmeal')
+sl.text('Kale, Spinach & Rocket Smoothie')
+sl.text('Hard-Boiled Free-Range Egg')
