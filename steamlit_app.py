293ecb600766ca100512d489619034a2d5dbfc44
raunakp777 <72513266+raunakp777@users.noreply.github.com>
2023-01-05 16:55:10 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -56,7 +56,7 @@ sl.stop()
 ##snowflake connections
 sl.header("The fruit load list contains:")
 #snowflake-related functions
-def get_fruit_load_list:
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
