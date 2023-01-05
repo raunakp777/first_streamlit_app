001670ff44557862c7888031b0b806d70a46359d
raunakp777 <72513266+raunakp777@users.noreply.github.com>
2023-01-05 13:56:25 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as sl
 import pandas as pd
+import requests as rq
 
 sl.title("My Mom's Healthy New Diner")
 
@@ -25,6 +26,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 sl.dataframe(fruits_to_show)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
 
