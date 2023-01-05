8aa6fc7b52c4e6bf7e77dc42049d92f91f02a978
raunakp777 <72513266+raunakp777@users.noreply.github.com>
2023-01-05 16:39:37 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -29,16 +29,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 sl.dataframe(fruits_to_show)
 
+#Create a function
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
 
+#New Section to display fruityvice api response
 sl.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = sl.text_input('What fruit would you like information about?')
   if not fruit_choice:
     sl.error("Please select a fruit to get information")
   else:
-    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    sl.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    sl.dataframe(back_from_function)
 
 except URLError as e:
    sl.error()
