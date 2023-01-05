b831d3552a0392e6eb25fcc55db9f43c37299062
raunakp777 <72513266+raunakp777@users.noreply.github.com>
2023-01-05 16:26:28 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -31,17 +31,19 @@ sl.dataframe(fruits_to_show)
 
 
 sl.header("Fruityvice Fruit Advice!")
-fruit_choice = sl.text_input('What fruit would you like information about?','Kiwi')
-sl.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = sl.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    sl.error("Please select a fruit to get information")
+   else:
+    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    sl.dataframe(fruityvice_normalized)
 
-
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take json version and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# output df as table
-sl.dataframe(fruityvice_normalized)
-
+except URLError as e:
+   sl.error()
+    
+    
 #dont run anything after
 sl.stop()
 
