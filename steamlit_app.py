80f1fb905bda3799f6acdd9173bfe89f9f75830c
raunakp777 <72513266+raunakp777@users.noreply.github.com>
2023-01-05 16:54:29 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -54,13 +54,19 @@ sl.stop()
 
 
 ##snowflake connections
-my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 sl.header("The fruit load list contains:")
-sl.dataframe(my_data_rows)
-
+#snowflake-related functions
+def get_fruit_load_list:
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+  
+#add a button to load the fruit
+if sl.button ('Get Fruit Load List'):
+ my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
+ my_data_rows = get_fruit_load_list()
+ sl.dataframe(my_data_rows)
+ 
 
 add_my_fruit = sl.text_input('What fruit would you like to add?','Jackfruit') 
 sl.write('Thanks for adding ', add_my_fruit)
