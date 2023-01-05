771d0d600efffde8ca8ddb4a7d47c02d13fee83c
raunakp777 <72513266+raunakp777@users.noreply.github.com>
2023-01-05 13:56:49 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -28,7 +28,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 sl.dataframe(fruits_to_show)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
 
